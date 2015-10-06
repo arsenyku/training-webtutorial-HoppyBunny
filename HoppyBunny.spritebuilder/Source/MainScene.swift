0104b07d0508e0069517d8bf254c95f3db5a6412
asu <arsenyk.ustaris.dev@gmail.com>
2015-10-05 19:14:34 -0700

    Bunny hops on screen tap

--- HoppyBunny.spritebuilder/Source/MainScene.swift
+++ HoppyBunny.spritebuilder/Source/MainScene.swift
@@ -1,5 +1,14 @@
 import Foundation
 
 class MainScene: CCNode {
-
+    weak var hero: CCSprite!
+ 
+    func didLoadFromCCB() {
+        userInteractionEnabled = true
+    }
+    
+    override func touchBegan(touch: CCTouch!, withEvent event: CCTouchEvent!) {
+        hero.physicsBody.applyImpulse(ccp(0, 400))
+    }
+    
 }
